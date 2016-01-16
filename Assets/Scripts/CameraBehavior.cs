01856821e8b42e4e58e49d585f1cde788ca7d26b
ryan-meyers <ryanmeyers03@gmail.com>
2016-01-16 05:04:14 -0600

    Camera Behavior Script

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour
+{
+
+	public GameObject player1;
+	public GameObject player2;
+
+	public Vector3 middle;
+
+	// Use this for initialization
+	void SetCamPos() 
+	{
+	
+		middle = (player1.transform.position + player2.transform.position)* 0.5f;
+
+		transform.position = new Vector3(
+			middle.x,
+			transform.position.y,
+			transform.position.z
+		);
+		
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+		SetCamPos();
+	}
+}
