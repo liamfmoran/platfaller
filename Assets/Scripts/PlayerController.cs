0b7d4d7ad3cb07f74d69f810f0d003501ee9b580
Liam Moran <liamfmoran@outlook.com>
2016-01-16 04:53:52 -0600

    Added arena and ball with basic movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    public bool jumpValue = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision Collision)
+    {
+        jumpValue = true;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement;
+        
+        if (Input.GetKeyDown(KeyCode.Space) && jumpValue == true)
+        {
+            movement = new Vector3(moveHorizontal, 10.0f, moveVertical);
+            jumpValue = false;
+        }
+        else
+        {
+            movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        }
+        rb.AddForce(movement * speed);
+    }
+}
