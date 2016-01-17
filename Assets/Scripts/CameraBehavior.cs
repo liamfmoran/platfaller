0af33120964e41f4dbc3b50c361cd17261ca93b5
coltonweaver <cbw1996@gmail.com>
2016-01-16 19:21:07 -0600

    Started scoring and finished spawning balls

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -7,6 +7,7 @@ public class CameraBehavior : MonoBehaviour
 	public GameObject player1;
 	public GameObject player2;
     public GameObject player3;
+    public GameObject player4;
 
 	public Vector3 middle;
 
@@ -14,7 +15,7 @@ public class CameraBehavior : MonoBehaviour
 	void SetCamPos() 
 	{
 
-        middle = (player1.transform.position * weight(player1.transform.position) + player2.transform.position * weight(player2.transform.position) + player3.transform.position * weight(player3.transform.position) + new Vector3(0f, 0f, 0f)) / 4f;
+        middle = (player1.transform.position * weight(player1.transform.position) + ((player2 != null) ? Vector3.zero : player2.transform.position * weight(player2.transform.position)) + ((player3 != null) ? Vector3.zero : player3.transform.position * weight(player3.transform.position)) + ((player4 != null) ? Vector3.zero : player4.transform.position * weight(player4.transform.position)) + new Vector3(0f, 0f, 0f)) / 4f;
 
 		transform.position = new Vector3(
 			middle.x,
